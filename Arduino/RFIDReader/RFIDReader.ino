501542ab26c0dc36ea9019be7e718fb9bf807823
dpoulson <darren.poulson@gmail.com>
2019-02-25 21:34:03 +0000

    Updated RFID to use TTGO board with built in speaker and 1.4 inch TFT

--- Arduino/RFIDReader/RFIDReader.ino
+++ Arduino/RFIDReader/RFIDReader.ino
@@ -1,32 +1,18 @@
 /**************************************************************************/
 /*! 
-    @file     readMifare.pde
-    @author   Adafruit Industries
-	@license  BSD (see license.txt)
+    @file     RFIDReader
+    @author   Darren Poulson
+	  @license  BSD (see license.txt)
 
-    This example will wait for any ISO14443A card or tag, and
-    depending on the size of the UID will attempt to read from it.
-   
-    If the card has a 4-byte UID it is probably a Mifare
-    Classic card, and the following steps are taken:
-   
-    - Authenticate block 4 (the first block of Sector 1) using
-      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
-    - If authentication succeeds, we can then read any of the
-      4 blocks in that sector (though only block 4 is read here)
-	 
-    If the card has a 7-byte UID it is probably a Mifare
-    Ultralight card, and the 4 byte pages can be read directly.
-    Page 4 is read by default since this is the first 'general-
-    purpose' page on the tags.
+    This sketch will wait for any ISO14443A card or tag programmed
+    up for the UK R2 Builders club
 
+    Results will be displayed on the attached LCD screen. Valid card IDs will
+    be sent to the R2 Builders driving course
 
-This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
-This library works with the Adafruit NFC breakout 
+This is based on the example sketch for the Adafruit PN532 NFC/RFID 
+breakout boards
   ----> https://www.adafruit.com/products/364
- 
-Check out the links above for our tutorials and wiring diagrams 
-These chips use SPI or I2C to communicate.
 
 Adafruit invests time and resources providing this open source code, 
 please support Adafruit and open-source hardware by purchasing 
@@ -34,12 +20,16 @@ products from Adafruit!
 
 */
 /**************************************************************************/
+#include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+#include <Adafruit_GFX.h>                                  // Core graphics library
+#include <Adafruit_ST7735.h>                               // Hardware-specific library
+
 #include "config.h"
 
 String droid_name;
@@ -48,73 +38,176 @@ String member_name;
 String member_uid;
 
 // If using the breakout with SPI, define the pins for SPI communication.
-#define PN532_SCK  (2)
-#define PN532_MOSI (12)
-#define PN532_SS   (4)
-#define PN532_MISO (13)
+#define PN532_SCK  (32)
+#define PN532_MOSI (27)
+#define PN532_SS   (21)
+#define PN532_MISO (26)
 
-#define BUZZER (5)
-#define LED (0)
-
-ADC_MODE(ADC_VCC);
+#define SPEAKER_PIN 25
+#define TONE_PIN_CHANNEL 0
 
 // Use this line for a breakout with a software SPI connection (recommended):
 Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+Adafruit_ST7735 tft = Adafruit_ST7735(16, 17, 23, 5, 9);
+WiFiMulti wifi;
 
-uint8_t last_uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+uint32_t _count;
+bool speaker_on;
+unsigned long lastRead;
+bool card_displayed;
+int tft_height, tft_width;
 
-void setup(void) {
-  pinMode(BUZZER, OUTPUT);
-  pinMode(LED, OUTPUT);
-  
+#define r2_logo_width 48
+#define r2_logo_height 60
+static unsigned char r2_logo_bits[] = {
+   0x00, 0x00, 0x00, 0x1d, 0x00, 0x00, 0x00, 0x00, 0xe0, 0xff, 0x01, 0x00,
+   0x00, 0x00, 0xf8, 0xff, 0x07, 0x00, 0x00, 0x00, 0x3e, 0xe0, 0x0f, 0x00,
+   0x00, 0x00, 0x06, 0x00, 0x1f, 0x00, 0x00, 0x80, 0x03, 0x00, 0x1e, 0x00,
+   0x00, 0x80, 0x01, 0x00, 0x38, 0x00, 0x00, 0xc0, 0x98, 0x00, 0x38, 0x00,
+   0x00, 0x60, 0xc8, 0x07, 0x70, 0x00, 0x00, 0x60, 0xc4, 0x3f, 0x70, 0x00,
+   0x00, 0x30, 0xc4, 0xff, 0x70, 0x00, 0x00, 0x30, 0xf4, 0xff, 0x73, 0x00,
+   0x00, 0x38, 0x00, 0xfe, 0x6f, 0x00, 0x00, 0x18, 0x00, 0xe0, 0x7f, 0x00,
+   0x00, 0x0e, 0x80, 0x03, 0x7f, 0x00, 0x00, 0x0f, 0xc0, 0x1f, 0x78, 0x00,
+   0x00, 0x07, 0xe0, 0x3f, 0xe0, 0x00, 0x80, 0x07, 0x30, 0x7c, 0x00, 0x01,
+   0x80, 0x03, 0x18, 0xf8, 0x00, 0x0e, 0xc0, 0x03, 0x18, 0xe0, 0x00, 0x00,
+   0xe0, 0x03, 0x18, 0xe0, 0x01, 0x00, 0xe0, 0x01, 0x1c, 0xc0, 0x01, 0x00,
+   0xe0, 0x01, 0x1c, 0xc0, 0x00, 0x00, 0xf0, 0x00, 0x1c, 0x00, 0x00, 0x00,
+   0x70, 0x00, 0x18, 0x00, 0x00, 0x00, 0x78, 0x00, 0x18, 0x00, 0x00, 0x00,
+   0x38, 0x00, 0x38, 0x80, 0x00, 0x00, 0x30, 0x00, 0x38, 0xc0, 0x00, 0x00,
+   0x18, 0x00, 0x70, 0xc0, 0x00, 0x00, 0x18, 0x00, 0x70, 0xe0, 0x00, 0x00,
+   0x0c, 0x00, 0xf0, 0xc0, 0x00, 0x00, 0x0c, 0x00, 0xe0, 0xe0, 0x00, 0x00,
+   0x06, 0x00, 0xe0, 0xe0, 0x00, 0x00, 0xc6, 0x01, 0xc0, 0xc1, 0x01, 0x00,
+   0xe0, 0x0f, 0xc0, 0xc1, 0x01, 0x00, 0xe0, 0x3f, 0x80, 0xc3, 0x01, 0x00,
+   0x00, 0xff, 0x81, 0xc1, 0x01, 0x00, 0x00, 0xff, 0x07, 0x81, 0x03, 0x00,
+   0x00, 0xe7, 0x1f, 0x80, 0x03, 0x00, 0x00, 0x8e, 0xff, 0x80, 0x07, 0x00,
+   0x00, 0x0e, 0xfc, 0x01, 0x07, 0x00, 0x00, 0x5c, 0xf0, 0x00, 0x0f, 0x00,
+   0x00, 0xfc, 0x07, 0x00, 0x1e, 0x00, 0x00, 0xfe, 0x0f, 0x00, 0x1e, 0x00,
+   0x80, 0xff, 0x1f, 0x00, 0x7c, 0x00, 0x80, 0x03, 0x0e, 0x00, 0x78, 0x00,
+   0xe0, 0x01, 0x08, 0x00, 0xf0, 0x01, 0x70, 0x00, 0x00, 0x00, 0xe0, 0x01,
+   0x38, 0x00, 0x00, 0x00, 0xc0, 0x07, 0x1c, 0x00, 0x00, 0x00, 0x80, 0x0f,
+   0x0c, 0x00, 0x00, 0x00, 0x00, 0x0f, 0x06, 0x00, 0x00, 0x00, 0x00, 0x1e,
+   0x06, 0x00, 0x00, 0x00, 0x00, 0x3c, 0x06, 0x00, 0x00, 0x00, 0x00, 0x38,
+   0xff, 0x07, 0x00, 0x00, 0x00, 0x38, 0xfe, 0x07, 0x00, 0x00, 0x00, 0x38,
+   0xfc, 0x03, 0x00, 0x00, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x80, 0xff, 0x0f,
+   0x00, 0x00, 0x00, 0x80, 0xff, 0x03, 0x00, 0x00, 0x00, 0x00, 0x37, 0x00 };
+   
+void tone(uint8_t frequency, uint8_t duration) {
+   ledcWriteTone(TONE_PIN_CHANNEL, frequency);
+   _count = millis() + duration;
+   speaker_on = 1;  
+}
+
+void drawScreen() {
+  tft.fillScreen(ST7735_BLACK); 
+  tft.setCursor(4,0);
+  tft.print("R2 Builders");
+  tft.setCursor(4,12);
+  tft.print("  Driving Course");
+  tft.drawFastHLine(0,24,200,ST7735_BLUE);
+  tft.drawXBitmap((tft_height/2)-(r2_logo_width/2),(tft_height/2)-(r2_logo_height/2), r2_logo_bits, r2_logo_width, r2_logo_height, 0x333333);
+  tft.setCursor(35,44);
+  tft.print("Please Swipe");
+  tft.setCursor(55,56);
+  tft.print("Tag");
+}
+
+void setup(void) {  
   Serial.begin(115200);
+  // Init Screen 
+  tft.initR(INITR_18GREENTAB);                             // 1.44 v2.1
+  tft.fillScreen(ST7735_BLACK);                            // CLEAR
+  tft.setTextColor(0x5FCC);                                // GREEN
+  tft.setRotation(1);
+  tft_height = tft.height();
+  tft_width = tft.width();
+  Serial.print("Width of screen: ");
+  Serial.println(tft_width);
+  Serial.print("Height of screen: ");
+  Serial.println(tft_height);
+  tft.drawXBitmap((tft_height/2)-(r2_logo_width/2),(tft_height/2)-(r2_logo_height/2), r2_logo_bits, r2_logo_width, r2_logo_height, 0xdd);
+
+  // Setup Speaker
+  ledcSetup(TONE_PIN_CHANNEL, 0, 13);
+  ledcAttachPin(SPEAKER_PIN, TONE_PIN_CHANNEL);  
+
+  // Connect to Wifi   
+  tft.setCursor(4,0);
+  tft.print("Connecting to Wifi");
+
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
+  //wiFi.mode(WIFI_STA);
+  wifi.addAP(ssid, pass);
+  while (wifi.run() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  nfc.begin();
+  Serial.print("   Connected");
+  tft.setCursor(4,12);
+  tft.print("Connected...");
 
+
+
+  // Initialise NFC Reader
+  tft.setCursor(4,24);
+  tft.print("Initialising Reader");
+  tft.setCursor(4,36);
+  nfc.begin();
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (! versiondata) {
     Serial.print("Didn't find PN53x board");
+    tft.print("FAILED!");
     while (1); // halt
   }
   // Got ok data, print it out!
   Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
   Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC); 
   // configure board to read RFID tags
-  nfc.SAMConfig();
-  
+  nfc.SAMConfig(); 
+  nfc.setPassiveActivationRetries(0x10);
+  tft.print("Done");
+  delay(500);
   Serial.println("Waiting for an ISO14443A Card ...");
+  
+  // Setup done
+  drawScreen();
+  tone(1000,100);
+  
 }
-
 
 void loop(void) {
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-    
+
+  // Mute Speaker
+  if(speaker_on) {
+     if(millis() > _count) {
+         speaker_on = 0;
+         Serial.println("Muting Speaker");
+         ledcWriteTone(TONE_PIN_CHANNEL, 0);
+         digitalWrite(SPEAKER_PIN, 0);
+     }
+  }
+
+  // Clear screen of names
+  if (millis() > lastRead + 2000 && card_displayed) {
+     card_displayed = 0;
+     drawScreen();
+  }    
   // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
   // 'uid' will be populated with the UID, and uidLength will indicate
   // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
   
-  if (success) {
+  if (success && lastRead + 2000 < millis()) {
+    lastRead = millis();
     // Display some basic information about the card
     Serial.println("Found an ISO14443A card");
     Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
     Serial.print("  UID Value: ");
     nfc.PrintHex(uid, uidLength);
-    Serial.println("");
-    Serial.print("  Last UID: ");
-    nfc.PrintHex(last_uid, uidLength);
-    Serial.println("");
     
     if (uidLength == 4)
     {
@@ -124,7 +217,6 @@ void loop(void) {
       // Now we need to try to authenticate it for read/write access
       // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
       Serial.println("Trying to authenticate block 4 with default KEYA value");
-      //uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
       uint8_t keya[6] = { 0xD3, 0xF7, 0xD3, 0xF7, 0xD3, 0xF7 };
 	  
 	  // Start with block 4 (the first block of sector 1) since sector 0
@@ -198,6 +290,11 @@ void loop(void) {
           Serial.println(member_name);
           Serial.print("Member UID: ");
           Serial.println(member_uid);
+          tft.fillRect(0,35,200,200,ST7735_BLACK);
+          tft.setCursor(4, 44);
+          tft.print(member_name);
+          tft.setCursor(4, 56);
+          tft.print(droid_name);
 
           String memberCall = String((char*)api) + "member/" + member_uid;
           String droidCall = String((char*)api) + "droid/" + droid_uid;
@@ -218,7 +315,8 @@ void loop(void) {
               Serial.println(payload);
             }
           } else {
-            Serial.println("Error");
+            Serial.print("Error: ");
+            Serial.println(httpCode);
           }
           http.end();
           Serial.println("Register droid");
@@ -235,32 +333,35 @@ void loop(void) {
               Serial.println(payload);
             }
           } else {
-            Serial.println("Error");
+            Serial.print("Error: ");
+            Serial.println(httpCode);
           }
            http.end();
 
           // Lets notify that a card was read ok
-          tone(BUZZER, 1000);
-          digitalWrite(LED, HIGH);
-          delay(500);
-          noTone(BUZZER);
-          digitalWrite(LED, LOW);
           // Wait a bit before reading the card again
-          delay(2000);
-          Serial.print("VCC Voltage: ");
-          Serial.println(ESP.getVcc());
+          tone(1000, 100);
+          card_displayed = 1;
         }
         else
         {
           Serial.println("Ooops ... unable to read the requested block.  Try another key?");
+          tone(600, 100);
+          tft.fillRect(0,35,200,200,ST7735_BLACK);
+          tft.setCursor(4, 44);
+          tft.print("Failed to read tag");
+          card_displayed = 1;
         }
       }
       else
       {
         Serial.println("Ooops ... authentication failed: Try another key?");
+        tone(400, 100);
+        tft.fillRect(0,35,200,200,ST7735_BLACK);
+        tft.setCursor(4, 44);
+        tft.print("Invalid Tag");
+        card_displayed = 1;
       }
-    }
-    
-    delay(1000);
+    }   
   }
 }
